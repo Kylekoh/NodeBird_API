0916042940a0c08e95c7f3dadf48e3185788c8e2
kyle_koh <ykoh728@gmail.com>
2018-11-05 23:22:38 +0900

    create domain model for API

--- models/index.js
+++ models/index.js
@@ -12,6 +12,8 @@ db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
 db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Domain = require('./domain')(sequelize, Sequelize);
+
 db.User.hasMany(db.Post);
 db.Post.belongsTo(db.User);
 db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
@@ -26,5 +28,7 @@ db.User.belongsToMany(db.User, {
   as: 'Followings',
   through: 'Follow',
 });
+db.User.hasMany(db.Domain);
+db.Domain.belongsTo(db.User);
 
 module.exports = db;
